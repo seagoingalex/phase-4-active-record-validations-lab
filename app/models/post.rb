af0e6a7f37a1a081394e137b0e977df4e4bd6368
Alex Calvert <alexanderajcalvert@gmail.com>
2021-07-20 17:02:50 -0700

    commit tree fe578cd65c62e98f17499348d674217c6bceec51

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # validates_with ClickbaitValidator
+    validate :clickbait
+
+    def clickbait
+        # if title.include?("Won't Believe")
+        if title.present? && title.include?("Won't Believe")
+        else errors.add(:title, "You must add pizazz to your title!")
+        end
+        
+        # unless title.include?( "Won't Believe")
+        #     errors.add(:title, "You must add pizazz to your title!")
+        # end
+    end
+
 end
+
+# class ClickbaitValidator < ActiveModel::Validator
+#     def validate(record)
+#         if record.title.include?("Won't Believe")
+#         else
+#             record.errors.add :base, "You must add pizazz to your title!"
+#         end
+#     end
+# end
